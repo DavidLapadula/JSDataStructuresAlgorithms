bd63ffb92a94df355e9fe862c534aca30ed3386f
DavidLapadula <lapadula.david@gmail.com>
2022-07-25 09:41:51 -0400

    [Big O] intro to Big O and algorithm analysis

---
+++ Big-O/index.js
@@ -0,0 +1,15 @@
+/**Good Code */
+
+// Readable, Scalable (Big O measures this), Predictable
+// JS has performance.now() to measure how many ms things take
+// Need to determine what happens when input grows; the slower it slows down, the better
+// Calculate operations performed to abstract performance analysis from machine
+
+/**O(n) */
+
+// Increases linearly based on the input. n items = n operations
+
+/**O(n) */
+
+// Always takes a constant time, or KNOWN number of steps. 
+// Even if multiple operations, still constant, so no matter the input runtime stays constant
