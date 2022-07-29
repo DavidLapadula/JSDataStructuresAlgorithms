935a133996e26e1e32beebc465ccd26272fbd649
DavidLapadula <lapadula.david@gmail.com>
2022-07-29 09:30:15 -0400

    [data-structures] intro information to data structures

---
+++ DataStructuresIntro/index.js
@@ -0,0 +1,18 @@
+// DS is just collection of values, Alg's just manipulate those values
+// Different types of data belong in different structures because data is more useful when it is organized properly
+
+/** How computers store */
+// CPU, RAM, Storage
+// CPU can access RAM much faster than non-volatile memory
+// CPU connected to memory controller than performs R/W to RAM
+// Called RAM because controller can access any address because it has a connection to each address
+// Tend to get information near by, closer to the CPU, faster it is
+// Tuned to get information that is close by
+// CPU also has a cache for most recent operations
+// RAM limitations prevent storing unlimited information
+
+/**Goal */
+// Minimize the amount of operations for getting or storing data
+
+/**Operations */
+// Access, Insertion, Deletion, Traversal, Searching, Sorting
