672fc818e8355d273d8b02106e407da368c3a4b4
DavidLapadula <lapadula.david@gmail.com>
2022-08-03 09:41:07 -0400

    [arrays] tested and used basic array operations

--- Arrays/index.js
+++ Arrays/index.js
@@ -26,3 +26,62 @@ strings.splice(2, 0, 'alien');
 // Reference type: objects saved by reference to memory unless otherwise specified
 // Context: where you are within object, value of 'this'. 'this' gets new value inside functions
 // Instantiation: create new copy of an object, instances or multiple copies of an object. super() allows you to call constructor of parent and get access to member variables
+
+/**Implementing an Array */
+
+// JS array is object with integer keys but behaves like an array
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {}; 
+    }
+
+    get(index) {
+        return this.data[index]; 
+    }
+
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length; 
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1]; // possible because array is object
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index){
+        const item = this.data[index];
+        this.shiftItems(index);
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1]; // shift all items to the left, item at the index is replaced
+        }
+        delete this.data[this.length - 1]; // could also just run the loop until less than OR EQUAL to length
+        this.length--;
+    }
+}
+
+/**Reverse a string */
+
+// Strings are arrays of characters; treat like an array if asked a question
+
+function reverse(str){
+    if (!str || str.length < 2 || typeof str !== 'string'){
+        return str;
+    }
+
+    const backwards = []; 
+    const totalItems = str.length - 1; 
+
+    for (let i = totalItems; i >= 0; i--) {
+        backwards.push(backwards.push(str[i])); 
+    }
+    return backwards.join();
+}
