d09d2298f14e3308a68083845482e7ab64f8d7e7
DavidLapadula <lapadula.david@gmail.com>
2022-08-04 22:46:58 -0400

    [arrays] simple solution to merge sorted arrays

--- Arrays/index.js
+++ Arrays/index.js
@@ -84,4 +84,46 @@ function reverse(str){
         backwards.push(backwards.push(str[i])); 
     }
     return backwards.join();
+
+    // return str.split('').reverse().join(''); cleaner solution with built in methods. run time is the same
 }
+
+/**Merge sorted arrays */
+// make functions readable, like they are english, by breaking checks into functions. Shows you know where to improve things
+
+function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+    let array1Item = array1[0];
+    let array2Item = array2[0];
+    let i = 1;
+    let j = 1;
+
+    if (array1.length === 0) {
+        return array2;
+    }
+
+    if (array2.length === 0) {
+        return array1;
+    }
+
+    while (array1Item || array2Item) {
+        if (!array2Item || array1Item < array2Item) {
+            mergedArray.push(array1Item);
+            console.log(array1Item);
+            array1Item = array1[i];
+            i++;
+            console.log(array1Item);
+        } else {
+            mergedArray.push(array2Item);
+            array2Item = array2[j];
+            j++;
+        }
+    }
+
+    return mergedArray;
+}
+
+mergeSortedArrays([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]);
+
+// Pros: access, fast push/pop, ordered
+// Cons: slow insert, slow delete, could be fixed size
