ba1d0ecd061209ca46a0ad78d6e27ec5d11b2f55
DavidLapadula <lapadula.david@gmail.com>
2022-08-09 09:43:53 -0400

    [linked-lists] add methods for append,prepend to Linked list class

--- LinkedLists/index.js
+++ LinkedLists/index.js
@@ -18,3 +18,39 @@
 
 /**Implement LL */
 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value){
+        this.head = new Node(value);
+        this.tail = this.head; // tail is how you track the last item, for appending
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode; // when its the first item, this.tail POINTS TO this.head
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(6);
+myLinkedList.prepend(20);
+console.log(myLinkedList);
