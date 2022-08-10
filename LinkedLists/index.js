bb6f565f303c152676d40073e10db1d592736f7f
DavidLapadula <lapadula.david@gmail.com>
2022-08-10 09:21:45 -0400

    [linked-list] implement insert and delete to LinkedList

--- LinkedLists/index.js
+++ LinkedLists/index.js
@@ -32,6 +32,28 @@ class LinkedList {
         this.length = 1;
     }
 
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(array);
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while(counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+
     append(value) {
         const newNode = new Node(value);
         this.tail.next = newNode; // when its the first item, this.tail POINTS TO this.head
@@ -47,10 +69,129 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+
+        if (index === 0) {
+            return this.prepend(value);
+        }
+
+        const newNode = new Node(value);
+        const leader = this.traverseToIndex(index - 1); // get the item before the place you want to insert
+        const holdingPoint = leader.next;
+
+        leader.next = newNode;
+        newNode.next = holdingPoint;
+    }
+
+    delete(index) {
+        if (index >= this.length) {
+            return; 
+        }
+
+        if (index === 0) {
+            this.head = this.head.next; // JS wil garbage collect head
+        }
+
+        const leader = this.traverseToIndex(index - 1);
+        const deleteNode = leader.next;
+
+        leader.next = deleteNode.next;
+        this.length--;
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(6);
 myLinkedList.prepend(20);
-console.log(myLinkedList);
+myLinkedList.insert(2, 105);
+myLinkedList.printList();
+
+/**Doubly LL */
+
+// Allow traversing list in reverse order because additional pointer to previous
+// Lookup could be O(n / 2) because knowing which half the index is in and can start on either end
+// Downside is more memory
+
+class DoublyLinkedList {
+    constructor(value){
+        this.head = new Node(value);
+        this.tail = this.head; // tail is how you track the last item, for appending
+        this.length = 1;
+    }
+
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        console.log(array);
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while(counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        this.tail.next = newNode; // when its the first item, this.tail POINTS TO this.head
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+
+        if (index === 0) {
+            return this.prepend(value);
+        }
+
+        const newNode = new Node(value);
+        const leader = this.traverseToIndex(index - 1); // get the item before the place you want to insert
+        const holdingPoint = leader.next;
+
+        leader.next = newNode;
+        newNode.next = holdingPoint;
+    }
+
+    delete(index) {
+        if (index >= this.length) {
+            return; 
+        }
+
+        if (index === 0) {
+            this.head = this.head.next; // JS wil garbage collect head
+        }
+
+        const leader = this.traverseToIndex(index - 1);
+        const deleteNode = leader.next;
+
+        leader.next = deleteNode.next;
+        this.length--;
+    }
+}
