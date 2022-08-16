1425f61e29b8137abf711ae8451ae8ed57552ed0
DavidLapadula <lapadula.david@gmail.com>
2022-08-16 09:51:08 -0400

    [binary-tree] implement insert,search,remove of BST

--- Trees/index.js
+++ Trees/index.js
@@ -30,3 +30,93 @@
 // Unbalanced, keep adding items to the right/left then turns into a LL. Turns lookup into O(n) which could be worst case
 // Pros: potential O(log n) performance, data is structured/relational/ordered, flexible memory allocation
 // Cons: No O(1) operations
+
+/** Implement BST */
+
+
+class Node {
+    constructor() {
+        this.left = null; 
+        this.right = null; 
+        this.value = value;
+    }
+}
+
+
+ // iteration methods choose between left and right, divvide and conquer method makes it O(log n)
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const node = new Node(value);
+
+        if (this.root === null) {
+            this.root = node;
+            return;
+        }
+
+        let currentNode = this.root;
+
+        while (true) {
+            if (value < currentNode.value) {
+                if (!currentNode.left) {
+                    currentNode.left = node;
+                    return this;
+                }
+
+                currentNode = currentNode.left;
+            }
+
+            if (value >= currentNode.value) { // implementers choice regarding what to do with equal values
+                if (!currentNode.right) {
+                    currentNode.right = node;
+                    return this;
+                }
+
+                currentNode = currentNode.right;
+            }
+        }
+
+    }
+
+    lookup(value) {
+        if (this.root === null) {
+            return false;
+        }
+
+        let currentNode = this.root;
+
+        while (currentNode) { // loop will eventually stop when right or left of a node has null value
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            }
+
+            if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            }
+
+            if (value === currentNode.value) {
+                return currentNode;
+            }
+        }
+
+        return false;
+    }
+
+    remove(value) {
+        if (this.root === null) {
+            return false;
+        }
+
+        let currentNode = this.root; 
+        let parentNode = null;
+
+        while (currentNode) {
+            
+        }
+    }
+}
+
+const tree = new BinarySearchTree();
