72b2acde87243e01f5f3e825cb68e5cd1095938f
DavidLapadula <lapadula.david@gmail.com>
2022-08-22 12:00:44 -0400

    [trees] implement BST remove method

--- Trees/index.js
+++ Trees/index.js
@@ -44,79 +44,154 @@ class Node {
 
 
  // iteration methods choose between left and right, divvide and conquer method makes it O(log n)
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
+ class BinarySearchTree {
+    constructor(){
+      this.root = null;
     }
-
-    insert(value) {
-        const node = new Node(value);
-
-        if (this.root === null) {
-            this.root = node;
-            return;
-        }
-
+    insert(value){
+      const newNode = new Node(value);
+      if (this.root === null) {
+        this.root = newNode;
+      } else {
         let currentNode = this.root;
-
-        while (true) {
-            if (value < currentNode.value) {
-                if (!currentNode.left) {
-                    currentNode.left = node;
-                    return this;
-                }
-
-                currentNode = currentNode.left;
+        while(true){
+          if(value < currentNode.value){
+            //Left
+            if(!currentNode.left){
+              currentNode.left = newNode;
+              return this;
             }
-
-            if (value >= currentNode.value) { // implementers choice regarding what to do with equal values
-                if (!currentNode.right) {
-                    currentNode.right = node;
-                    return this;
-                }
-
-                currentNode = currentNode.right;
-            }
+            currentNode = currentNode.left;
+          } else {
+            //Right
+            if(!currentNode.right){
+              currentNode.right = newNode;
+              return this;
+            } 
+            currentNode = currentNode.right;
+          }
         }
-
+      }
     }
-
-    lookup(value) {
-        if (this.root === null) {
-            return false;
-        }
-
-        let currentNode = this.root;
-
-        while (currentNode) { // loop will eventually stop when right or left of a node has null value
-            if (value < currentNode.value) {
-                currentNode = currentNode.left;
-            }
-
-            if (value > currentNode.value) {
-                currentNode = currentNode.right;
-            }
-
-            if (value === currentNode.value) {
-                return currentNode;
-            }
-        }
-
+    lookup(value){
+      if (!this.root) {
         return false;
+      }
+      let currentNode = this.root;
+      while(currentNode){
+        if(value < currentNode.value){
+          currentNode = currentNode.left;
+        } else if(value > currentNode.value){
+          currentNode = currentNode.right;
+        } else if (currentNode.value === value) {
+          return currentNode;
+        }
+      }
+      return null
     }
-
     remove(value) {
-        if (this.root === null) {
-            return false;
+      if (!this.root) {
+        return false;
+      }
+      
+      let currentNode = this.root;
+      let parentNode = null;
+
+      while(currentNode) {
+        // Objective 1: Traverse tree to find the value
+        if(value < currentNode.value){
+          parentNode = currentNode;
+          currentNode = currentNode.left;
         }
+        
+        if (value > currentNode.value){
+          parentNode = currentNode;
+          currentNode = currentNode.right;
+        }
+        
+        // Value found
+        if (currentNode.value === value) {
+          // Objective 2: Find the next biggest value after the item being removed
 
-        let currentNode = this.root; 
-        let parentNode = null;
+          // No right child means next biggest is left child so point parent at left to skip over item and remove it
+          if (currentNode.right === null) {
+            if (parentNode === null) {
+              this.root = currentNode.left;
+            }
 
-        while (currentNode) {
+            if(currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } 
             
+            if(currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+
+          // Has a right child but that child has no values smaller than it, right child is next biggest
+          if (currentNode.right.left === null) {
+            currentNode.right.left = currentNode.left; // make left tree of current (smaller than it) the left tree of right child, which is next biggest
+            
+            if(parentNode === null) {
+              this.root = currentNode.right;
+            }
+            
+            // Checking to see which side of the parent node we are on, and then point the correct side of the parent tree to the next biggest item, which is current node.right
+            if(currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            }
+              
+            if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.right;
+            }
+          }
+
+
+          // Need to find leftest leaf node, which would be the next biggest
+          if (currentNode.right !== null && currentNode.right.left !== null) {
+              let leftmost = currentNode.right.left;
+              let leftmostParent = currentNode.right;
+              
+              // traverse down the tree to find leftmost
+              while (leftmost.left !== null) {
+                leftmostParent = leftmost;
+                leftmost = leftmost.left;
+              }
+              
+              leftmostParent.left = leftmost.right; // make right side of next biggest item left side of the parent
+              // replace removal item subtree(s) with those of next biggest
+              leftmost.left = currentNode.left;
+              leftmost.right = currentNode.right; 
+    
+              // remove item by pointing parent at next biggest
+              if(parentNode === null) {
+                this.root = leftmost;
+              } else {
+                if(currentNode.value < parentNode.value) {
+                  parentNode.left = leftmost;
+                } else if(currentNode.value > parentNode.value) {
+                  parentNode.right = leftmost;
+                }
+              }
+          }
+
+          return true;
         }
+      }
     }
-}
+  }
 
 const tree = new BinarySearchTree();
+
+/**AVL BST */
+// Balanced: 2 siblings never differ by > 1 level. No leaf should have much longer path to root node compared to other leaves
+// Subtrees of h1, h2 then |h1 - h2| <= 1 to be balances. Applies to ALL subtrees, even those without one child
+// More rotations, but more balanced
+
+/**R-B BST */
+// Adheres to rules to remain balances and ensure O(log n). Rules relating to nodes painted red/black
+// Rotations allow the tree to remain balanced by following the rules
+// Less balanced, but less rotations, so less expensive
+// 1 extra bit per node needed for the color
+
+/**Binary Heap */
