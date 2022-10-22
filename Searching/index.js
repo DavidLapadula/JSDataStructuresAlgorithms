a3d9932c586c89d31817c1565775ab56a6cc97d0
DavidLapadula <lapadula.david@gmail.com>
2022-10-22 11:54:07 -0400

    [searching] intro to BFS and DFS

---
+++ Searching/index.js
@@ -0,0 +1,17 @@
+/**Linear search */
+
+// Find target value in list by checking each value sequentially. 
+// O(1) best and O(n) worst case
+
+/**Binary search */
+
+// Divide and conquer, so log n time
+// List needs to be sorted, and can then be better than O(n), b/c discarding half the items every run
+// Data stored in a tree is more efficient
+
+/**Graph and tree traversal */
+
+// If traversal visits every node then must be linear time
+// Better than list because graph/tree avoids linear search
+// Better than array because faster insertion/deletion
+// Better than a hashtable because hashtables are not ordered
