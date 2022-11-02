0a0f797458108afb971a13d9d0054957c85ad8ad
DavidLapadula <lapadula.david@gmail.com>
2022-11-01 22:44:42 -0400

    [searching] start implementing BFS and DFS

--- Searching/index.js
+++ Searching/index.js
@@ -15,3 +15,17 @@
 // Better than list because graph/tree avoids linear search
 // Better than array because faster insertion/deletion
 // Better than a hashtable because hashtables are not ordered
+
+/** BFS */
+
+// Go horizontally across tree
+// Uses more memory because tracks nodes already visited and children 
+// Shortest path between nodes because searches closest first
+// Good to use if know that node is in top of tree
+
+/** DFS */
+
+// Search follows branch all the way down, and then goes to nearest ancestor with unexplored child
+// Less memory because do not need to store pointer to children
+// Good for checking if path exists because always exploreing children
+// Useful when node looking for might be at the bottom
